d382ef22baca7a82b2ade656b6f3724e675f04ea
PioneerMNDR <Kromov76@mail.ru>
2025-06-02 18:35:58 +0300

    Added functional module telegram bot

--- src/Classes/Services/AdvancedQueryService.cs
+++ src/Classes/Services/AdvancedQueryService.cs
@@ -2,6 +2,9 @@
 using MousyHub.Models.Misc;
 using MousyHub.Models.Model;
 using MousyHub.Classes.Model;
+using MousyHub.Classes.User;
+using DocumentFormat.OpenXml.Bibliography;
+using static MudBlazor.CategoryTypes;
 namespace MousyHub.Models.Services
 {
     public class AdvancedQueryService
@@ -10,7 +13,7 @@ namespace MousyHub.Models.Services
         public AdvancedQueryService(ProviderService providerService)
         {
             _providerService = providerService;
-        
+
         }
         public double CustomTemperature = 1.0;
         public int CustomMaxTokens = 300;
@@ -21,7 +24,7 @@ namespace MousyHub.Models.Services
         {
             isInstructed = _providerService.SelectType.Key is ProviderService.APIType.Cloud && isChatCompletions ? false : true;
         }
-        public async Task<MessageResponse> Generate(string SystemPromt, string UserPromt, GenerationConfig generationConfig, Instruct instruct, string BotName = "Assistant", string UserName = "User", string PromtAfterOutputSequence = "", bool isChatCompletions=false)
+        public async Task<MessageResponse> Generate(string SystemPromt, string UserPromt, GenerationConfig generationConfig, Instruct instruct, string BotName = "Assistant", string UserName = "User", string PromtAfterOutputSequence = "", bool isChatCompletions = false)
         {
             DetectAPIType(isChatCompletions);
             if (_providerService.Status)
@@ -59,5 +62,29 @@ namespace MousyHub.Models.Services
             }
             return new MessageResponse("", false, "No connection");
         }
+
+        public async Task<MessageResponse> GenerateWithExistingChat(string UserPromt, ChatState chatState, SettingsService settings, bool isChatCompletions = false)
+        {
+            DetectAPIType(isChatCompletions);
+            if (_providerService.Status && chatState.ChatHistory is not null)
+            {
+                var person = await chatState.AddNewUserMessageInChat(UserPromt);
+                if (person.IsUser)
+                {
+                    throw new Exception("Queue error in AdvancedQueryService");
+                }
+                string stop_seq = settings.CurrentInstruct.stop_sequence + StringHelperBuilder.TagPlaceholder(",{{user}}:,{{char}}:", chatState.ChatHistory.MainUser.Name, chatState.ChatHistory.MainCharacter.Name);
+                Promt promt = new Promt(chatHistory: chatState.ChatHistory, instruct: settings.CurrentInstruct, person, reasoningOptions: settings.User.ReasoningOptions, isChatCompletions);
+                Console.WriteLine("Query Promt: " + promt.FullContent);
+
+                var res =  await _providerService.LLModel.GenerateTextAsync(promt, settings.CurrentGenerationConfig, maxTokens: settings.User.CurrentMaxToken, stop_sequence: stop_seq);
+                chatState.NextPerson = chatState.ChatHistory.QueueMoveOrder();
+                return res;
+            }
+            return new MessageResponse("", false, "No connection or open chat history");
+
+        }
+
+
     }
 }
